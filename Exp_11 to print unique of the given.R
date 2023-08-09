ad0c6a31263e0abad3cef53330bb08e00da21af3
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2023-08-09 08:14:55 +0530

    Add files via upload

---
+++ Exp_11 to print unique of the given.R
@@ -0,0 +1,8 @@
+s <- readline("Enter a string: ")
+cat("The unique elements of the string are:\n")
+print(unique(s))
+v <- sample(1:10, 10, replace = TRUE)
+cat("The vector is:\n")
+print(v)
+cat("The unique numbers of the vector are:\n")
+print(unique(v))
