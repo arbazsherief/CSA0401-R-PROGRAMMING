713f603271aa2dbb3270a4c4e093ff8fb47f0709
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2023-08-09 08:11:13 +0530

    Add files via upload

---
+++ Exp_1 name,age.R
@@ -0,0 +1,7 @@
+name <- readline("Enter your name: ")
+age <- as.integer(readline(prompt = "Enter your age: "))
+cat("Your name is", name, "\n") 
+cat("your age is", age, "\n")
+cat("The version of R installation is:\n")
+print(version)
+
