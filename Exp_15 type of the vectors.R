08c7d4c20d66da3d7f43c1f3acf94108aa099de7
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2023-08-09 08:16:29 +0530

    Add files via upload

---
+++ Exp_15 type of the vectors.R
@@ -0,0 +1,10 @@
+
+num_vec <- c(1, 2, 3, 4, 5)
+char_vec <- c("a", "b", "c", "d", "e")
+log_vec <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
+print(num_vec)
+print(char_vec)
+print(log_vec)
+typeof(num_vec)
+typeof(char_vec)
+typeof(log_vec)
